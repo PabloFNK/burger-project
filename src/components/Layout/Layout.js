9e94dc5f9fc2488b807f42afc19016e0194dd3cc
PabloFNK <pablo.fnk.foncas@gmail.com>
2018-03-14 23:08:27 +0100

    Starting Implementation of The Burger Builder Container

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AuxComp from 'hoc/Aux';
+
+import classes from './Layout.css';
+
+const layout = (props) => (
+    <AuxComp>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.content}>
+            {props.children}
+        </main>
+    </AuxComp>
+);
+
+export default layout;
